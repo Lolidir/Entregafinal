57996c70f7429a54dc33825970754598cc1973fd
Federico Lanciotti <fedelanciotti91@gmail.com>
2024-07-22 23:41:31 -0300

    form

--- js/app.js
+++ js/app.js
@@ -1,11 +1,14 @@
-let nombre
-let apellido
-
 let boton = document.querySelector('button')
 let formulario = document.querySelector('form')
 
 formulario.addEventListener('submit', (evento) => {
     evento.preventDefault()
     console.log('Nombre' ,formulario[0].value)
-    console.log('Edad' ,formulario[1].value)
+    console.log('Días' ,formulario[1].value)
+    console.log('Personas' ,formulario[2].value)
+    console.log('Alojamiento' ,formulario[3].value)
+    
 })
+
+
+
